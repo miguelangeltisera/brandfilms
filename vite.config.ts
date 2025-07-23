7099fdd30f26b9d8f5fa632d9d34de7767f3c514
Miguelangel Tisera <miguelangeltisera@gmail.com>
2025-07-23 15:23:01 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  },
   server: {
     port: 3000
   },
-  root: '.', // Explicitamente definir la raíz
+  build: {
+    outDir: 'dist'
+  }
 })
